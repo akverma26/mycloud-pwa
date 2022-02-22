32aaa4387661f94c3c42389fe68875f6e4b8bd3c
Ankit Kumar <akvindian26@gmail.com>
2022-02-23 01:44:37 +0530

    Code clean

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// const SERVER_URL = "http://localhost:3000/push-notification";
-const SERVER_URL = "https://akvcloudapi.herokuapp.com/push-notification";
+// const SERVER_URL = "http://localhost:3000";
+const SERVER_URL = "https://akvcloudapi.herokuapp.com";
 const VAPID_PUBLIC_KEY = "BHO_h5mRkL8oNzXk-SEZ2B8NCGJswQi5Ju7swcNipQ7v_ISPRJD3fhfM8UCMi9nHmSApGdMxU-Hy2ssFDrEBHT0";
 const CACHE_NAME = 'my-cloud-cache';
 const SUBSCRIPTION_TYPE = 'default';
@@ -17,7 +17,7 @@ const saveSubscriptionToServer = async () => {
     }
     console.log(JSON.stringify(body));
     fetch(
-        `${SERVER_URL}/save-subscription`, {
+        `${SERVER_URL}/push-notification/save-subscription`, {
         method: 'post',
         headers: {
             'Content-Type': 'application/json',
@@ -29,20 +29,6 @@ const saveSubscriptionToServer = async () => {
             console.log(res);
         })
         .catch((err) => { console.log(err) });
-}
-
-const showNotificationSW = (title, options = {}) => {
-    let _options = {
-        body: "This is notification body",
-        icon: "../../images/icons/seo128x128.png",
-        vibrate: [1000, 500, 1000, 500, 1000, 500, 1000, 500],
-        data: {
-            dateOfArrival: Date.now(),
-            primaryKey: 1,
-        },
-        ...options
-    };
-    self.registration.showNotification(title, _options);
 }
 
 const deleteCache = () => {
@@ -57,6 +43,68 @@ const deleteCache = () => {
     })
 }
 
+// ===========================================================
+// ================ Web Push Notification ====================
+// ===========================================================
+
+// Reference:
+// https://developers.google.com/web/ilt/pwa/introduction-to-push-notifications#display_a_notification
+// https://developer.mozilla.org/en-US/docs/Web/API/ServiceWorkerRegistration/showNotification
+
+// Show web push notification on the device
+const showWebPushNotification = (title = "Notification Title!", options = {}) => {
+    let _options = {
+        body: "Notification body.",
+        icon: "../../images/icons/seo128x128.png",
+        vibrate: [1000, 500, 1000, 500, 1000, 500, 1000, 500],
+        data: {
+            dateOfArrival: Date.now(),
+            primaryKey: 1,
+        },
+        actions: [{
+                action: 'explore',
+                title: 'View'
+            },
+            {
+                action: 'close',
+                title: 'Close'
+            },
+        ],
+        ...options
+    };
+    if (Notification.permission == 'granted') {
+        self.registration.showNotification(title, _options);
+    } else {
+        console.log("Notification permission is not granted.");
+    }
+}
+
+// Event listener for notification close
+self.addEventListener('notificationclose', function (e) {
+    // var notification = e.notification;
+    // var primaryKey = notification.data.primaryKey;
+});
+
+// Event listener for notification click
+self.addEventListener('notificationclick', function (e) {
+    var notification = e.notification;
+    var action = e.action;
+    if (action === 'close') {
+        notification.close();
+    } else {
+        // clients.openWindow('http://www.example.com');
+        clients.openWindow('/');
+        notification.close();
+    }
+});
+
+// ============================================================
+// ================ Service Worker Listeners ==================
+// ============================================================
+
+// Event listener, triggers when app is installed on the system.
+// For now I am doing nothing, but I have added a template code
+// to manage cache.
 self.addEventListener('install', function (event) {
     console.log('Installing...')
     event.waitUntil(
@@ -68,8 +116,22 @@ self.addEventListener('install', function (event) {
     );
 });
 
+// Event listener, triggers when service worker gets registered
+self.addEventListener('activate', async (event) => {
+    console.log("Service worker getting activated...");
+    try {
+        saveSubscriptionToServer();
+        event.waitUntil(deleteCache());
+    } catch (err) {
+        console.log(err)
+    }
+});
+
+// Event listener, triggers when app requests for any resource
 self.addEventListener('fetch', event => {
     if (event.request.method != 'GET') return;
+    if (event.request.url.startsWith(SERVER_URL)) return;
+    console.log(event.request.method, event.request.url);
     event.respondWith(
         // On Get fetch request try to serve it from out cache
         caches.open(CACHE_NAME).then((cache) => {
@@ -88,17 +150,9 @@ self.addEventListener('fetch', event => {
     );
 });
 
-self.addEventListener('activate', async (event) => {
-    try {
-        saveSubscriptionToServer();
-        event.waitUntil(deleteCache());
-    } catch (err) {
-        console.log(err)
-    }
-});
-
+// Event listener, triggers when we get push notification from the server
 self.addEventListener("push", async (event) => {
     console.log("Push received...");
     let data = event.data.json();
-    showNotificationSW(data.title, data.options);
+    showWebPushNotification(data.title, data.options);
 });
--- sw_registration.js
+++ sw_registration.js
@@ -1,11 +1,17 @@
+/* This file is just for registering service worker with our web app.
+*/
 const SERVICE_WORKER_PATH = './sw.js';
 
 let serviceWorkerRegistration = null;
 
+/* Register service worker as soon as page starts loading
+*/
 window.addEventListener("load", () => {
     registerServiceWorker();
 });
 
+/* Get service worker registration if it is already registered
+*/
 const getSWRegistration = () => {
     navigator.serviceWorker.getRegistration(SERVICE_WORKER_PATH)
         .then((sw_registration) => {
@@ -15,6 +21,7 @@ const getSWRegistration = () => {
         })
 }
 
+// Register the service worker
 const registerServiceWorker = () => {
     navigator.serviceWorker.register(SERVICE_WORKER_PATH)
         .then((sw_registration) => {
@@ -26,6 +33,7 @@ const registerServiceWorker = () => {
         });
 }
 
+// Unregister the service worker
 const unRegisterServiceWorker = () => {
     if (serviceWorkerRegistration) {
         serviceWorkerRegistration.unregister().then((unregistered) => {
@@ -37,6 +45,8 @@ const unRegisterServiceWorker = () => {
     }
 }
 
+// This is just to show status of service worker registration on our
+// main DOM, may be I'll remove these later
 document
     .querySelector(".register-service-worker button#get-status")
     .addEventListener("click", (event) => {
